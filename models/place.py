395be4451f8f4b6fd8f468829fe46a62e35ce5ce
tync0 <isgenderlituncay2004@gmail.com>
2024-03-29 16:21:05 +0400

    checking db or fb

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
@@ -45,3 +46,34 @@ class Place(BaseModel, Base):
 
     amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
     amenity_ids = []
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+
+        @property
+        def reviews(self):
+            """Getter Function for FileStorage"""
+            from models import storage
+
+            result = []
+            all_reviews = storage.all(Review).values()
+            for review in all_reviews:
+                if review.place_id == self.id:
+                    result.append(review)
+            return result
+
+        @property
+        def amenities(self):
+            """Getter Function for FileStorage"""
+            from models import storage
+
+            result = []
+            all_amenities = storage.all(Amenity).values()
+            for amenity in all_amenities:
+                if amenity.id in self.amenity_ids:
+                    result.append(amenity)
+            return result
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter Function for FileStorage"""
+            if type(obj) is Amenity:
+                self.amenity_ids.append(obj.id)
