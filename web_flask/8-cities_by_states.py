c636e69998c0398f6d997351006f187097e82fbf
aishalieva0 <aysalieva6@gmail.com>
2024-04-15 09:07:38 -0700

    Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    cities = storage.all(State)
+    return render_template('8-cities_by_states.html', cities=cities)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
