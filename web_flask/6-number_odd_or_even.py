fda8a203284fb61da9f42f59b84dd84340a67210
tync0 <isgenderlituncay2004@gmail.com>
2024-04-21 18:12:15 +0400

    validation

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,8 +45,7 @@ def number_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    odd_or_even = "odd" if n % 2 != 0 else "even"
-    return render_template("6-number_odd_or_even.html", n=n, odd_or_even=odd_or_even)
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
